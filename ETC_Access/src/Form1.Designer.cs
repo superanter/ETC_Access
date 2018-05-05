667659d6d8e8e1d61ce0ea00128fd279971a6fb8
superanter <anter@qq.com>
2018-05-05 22:45:56 +0800

    第一次上传

---
+++ ETC_Access/src/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ETC_Access
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAddRount = new System.Windows.Forms.Button();
+            this.txtA003_2 = new System.Windows.Forms.TextBox();
+            this.txtA003_3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 26);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(585, 206);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAddRount
+            // 
+            this.btnAddRount.Location = new System.Drawing.Point(556, 251);
+            this.btnAddRount.Name = "btnAddRount";
+            this.btnAddRount.Size = new System.Drawing.Size(75, 23);
+            this.btnAddRount.TabIndex = 1;
+            this.btnAddRount.Text = "增加";
+            this.btnAddRount.UseVisualStyleBackColor = true;
+            this.btnAddRount.Click += new System.EventHandler(this.btnAddRount_Click);
+            // 
+            // txtA003_2
+            // 
+            this.txtA003_2.Location = new System.Drawing.Point(165, 254);
+            this.txtA003_2.Name = "txtA003_2";
+            this.txtA003_2.Size = new System.Drawing.Size(100, 21);
+            this.txtA003_2.TabIndex = 2;
+            // 
+            // txtA003_3
+            // 
+            this.txtA003_3.Location = new System.Drawing.Point(433, 254);
+            this.txtA003_3.Name = "txtA003_3";
+            this.txtA003_3.Size = new System.Drawing.Size(100, 21);
+            this.txtA003_3.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 254);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 334);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtA003_3);
+            this.Controls.Add(this.txtA003_2);
+            this.Controls.Add(this.btnAddRount);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAddRount;
+        private System.Windows.Forms.TextBox txtA003_2;
+        private System.Windows.Forms.TextBox txtA003_3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ ETC_Access/src/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ETC_Access
+{
+    public partial class Form1 : Form
+    {
+        DataSet thisDataSet = new DataSet();
+        OleDbConnection con = new OleDbConnection("Provider = Microsoft.Jet.OLEDB.4.0; Data Source = \".\\mdb\\EtcAccess.mdb\"");
+        int[][] Temp = new int[3][];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void DataConnect()
+        {
+
+            thisDataSet.Reset();
+
+            OleDbDataAdapter thisAdapter1 = new OleDbDataAdapter("Select * From A001 Order by c_ID", con);
+            OleDbDataAdapter thisAdapter2 = new OleDbDataAdapter("Select * From A002 Order by c_ID", con);
+            OleDbDataAdapter thisAdapter3 = new OleDbDataAdapter("Select * From A003 Order by c_ID", con);
+
+            thisAdapter1.Fill(thisDataSet, "A001");
+            thisAdapter2.Fill(thisDataSet, "A002");
+            thisAdapter3.Fill(thisDataSet, "A003");
+
+            thisDataSet.Tables["A003"].Columns["c_ID"].AutoIncrement = true;
+
+            DataRelation thisA001toA002 = thisDataSet.Relations.Add("A001toA002",
+                thisDataSet.Tables["A001"].Columns["c_ID"],
+                thisDataSet.Tables["A002"].Columns["c_A001_ID"]);
+
+            DataRelation thisA002toA003 = thisDataSet.Relations.Add("A002toA003",
+                thisDataSet.Tables["A002"].Columns["c_ID"],
+                thisDataSet.Tables["A003"].Columns["c_A002_ID"]);
+
+            thisDataSet.Tables.Add("B001");
+            thisDataSet.Tables["B001"].Columns.Add("序号");
+            thisDataSet.Tables["B001"].Columns.Add("高速级别");
+            thisDataSet.Tables["B001"].Columns.Add("高速分类");
+            thisDataSet.Tables["B001"].Columns.Add("高速编号");
+            thisDataSet.Tables["B001"].Columns.Add("高速名称");
+
+            con.Close();
+
+            DataTable thisDataTable = thisDataSet.Tables["B001"];
+            dataGridView1.DataSource = thisDataTable;
+        }
+
+        private void SeachNomber(string strString)
+        {
+            thisDataSet.Tables["B001"].Clear();
+
+            foreach (DataRow x_A001 in thisDataSet.Tables["A001"].Rows)
+            {
+                foreach (DataRow x_A002 in x_A001.GetChildRows(thisDataSet.Relations["A001toA002"]))
+                {
+                    foreach (DataRow x_A003 in x_A002.GetChildRows(thisDataSet.Relations["A002toA003"]))
+                    {
+                        if (strString == "" || x_A003["c_Nomber"].ToString().StartsWith(strString.ToString()))
+                        {
+                            object[] rowVals = new object[5]
+                            {
+                                x_A003["c_ID"],
+                                x_A001["c_Name"],
+                                x_A002["c_Name"],
+                                x_A003["c_Nomber"],
+                                x_A003["c_Name"]
+                            };
+
+                            thisDataSet.Tables["B001"].Rows.Add(rowVals);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnAddRount_Click(object sender, EventArgs e)
+        {
+            if (txtA003_2.Text != "" && txtA003_3.Text != "")
+            {
+                string sql = "INSERT into A003 (c_A002_ID,c_Nomber,c_Name) VALUES ('" + Temp[1][comboBox1.SelectedIndex].ToString() + "','" + txtA003_2.Text.ToString() + "','" + txtA003_3.Text.ToString() + "')";
+                OleDbCommand cmd = new OleDbCommand(sql, con);
+                con.Open();
+                int aaa = cmd.ExecuteNonQuery();
+                con.Close();
+
+                DataConnect();
+                SeachNomber("");
+
+                txtA003_2.Text = "";
+                txtA003_3.Text = "";
+                MessageBox.Show("添加成功");
+            }
+            else
+            {
+                MessageBox.Show("无数据");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataConnect();
+            SeachNomber("");
+
+            object[] strTemp = new object[thisDataSet.Tables["A002"].Rows.Count];
+            Temp[1] = new int[thisDataSet.Tables["A002"].Rows.Count];
+            int t = 0;
+            foreach (DataRow x_A002 in thisDataSet.Tables["A002"].Rows)
+            {
+                strTemp[t] = x_A002[2];
+                Temp[1][t] = (int)x_A002[0];
+                t++;
+            }
+            comboBox1.DataSource = strTemp;
+        }
+    }
+}
